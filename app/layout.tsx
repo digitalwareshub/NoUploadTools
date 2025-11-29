c66f46940cbb6eac6431f75f82f369b471835f07
digitalwareshub <digitalwareshub@gmail.com>
2025-11-29 07:21:07 +0530

    Add Microsoft Clarity tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,6 +100,16 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "udgda5loaz");
+          `}
+        </Script>
       </head>
       <body>
         <Layout>{children}</Layout>
